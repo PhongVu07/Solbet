2794ceb57c38bec6a677ed8e1ed15b771dc518ca
Phong Vu <caophong.w@gmail.com>
2022-05-26 01:12:54 +0700

    Modify hook

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -3,23 +3,10 @@ import { Button, Col, Row, Typography } from "antd";
 import { Link } from "react-router-dom";
 import { ComponentContainer, OptionCard } from "./style.home";
 import { PAGES } from "constants/pages";
-import { Connection, PublicKey } from "@solana/web3.js";
-
-import { usePool } from "temp";
 
 const { Title, Paragraph } = Typography;
 
 const Home = () => {
-  let connection: Connection = new Connection(
-    "https://api.devnet.solana.com",
-    "processed"
-  );
-
-  usePool({
-    poolAddress: new PublicKey("93LyS9Y5ZugNuEUS5CeVLTkRYPeMMoA5obm8Mv8uen11"),
-    connection,
-  });
-
   return (
     <ComponentContainer>
       <Title>Welcome to Gold Pool</Title>
--- app/src/pages/PoolDetail/index.tsx
+++ app/src/pages/PoolDetail/index.tsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Input } from "antd";
+import { ComponentContainer } from "./style";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { useConnectedWallet } from "@saberhq/use-solana";
+
+import { usePool } from "temp";
+
+const { Search } = Input;
 
 const PoolDetail: React.FC = () => {
-  return <>Pool Detail</>;
+  const [poolPubkey, setPoolPubkey] = useState<string>("");
+  const wallet = useConnectedWallet();
+
+  const connection: Connection = new Connection(
+    "https://api.devnet.solana.com"
+  );
+  const {
+    userStakeAccount,
+    poolDetail,
+    setReload,
+    stake,
+    unstake,
+    claim,
+    getPendingReward,
+  } = usePool({
+    poolAddress: poolPubkey ? new PublicKey(poolPubkey) : PublicKey.default,
+    connection,
+    wallet,
+  });
+
+  return (
+    <ComponentContainer>
+      <Search
+        placeholder="Input Pool Public Key"
+        allowClear
+        enterButton
+        size="middle"
+        onSearch={(value) => setPoolPubkey(value)}
+      />
+    </ComponentContainer>
+  );
 };
 export default PoolDetail;
